89b28334ef626e015f0a276c14b8e775a2eb6ddb
botoddly <250804054+botoddly@users.noreply.github.com>
2026-08-27 08:26:01 +0200

    Reference SDL video driver identifier contract

--- src/Pixely/PlatformInfo.cs
+++ src/Pixely/PlatformInfo.cs
@@ -1,5 +1,6 @@
 namespace Pixely;
 
+// SDL video driver names are low-ASCII identifiers: https://wiki.libsdl.org/SDL3/SDL_GetVideoDriver
 public sealed record PlatformInfo(string? SdlVideoDriver)
 {
     public bool SupportsAlwaysOnTopWindows
